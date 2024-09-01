91cae7f41f8777042609f98f055578109e7569c4
tattypaw <67539605+tattypaw@users.noreply.github.com>
2024-09-01 09:00:00 +0800

    Update module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -1,17 +1,15 @@
-
 def get_matrix(n, m, value):
-    matrix=[]
+    matrix = []
     for i in range(n):
-        list_=[]
+        list_ = []
         for j in range(m):
             list_.append(value)
         matrix.append(list_)
-
     return matrix
 
-n=3 # задаем количество строк
-m=5 # задаем количество столбцов
-list_=get_matrix(n,m,"Yes!") # выполняем функцию get_matrix
+n = 3 # задаем количество строк
+m = 5 # задаем количество столбцов
+list_ = get_matrix(n, m, "Yes!") # выполняем функцию get_matrix
 # выводим список в виде матрицы (построчно)
 for i in range(n):
     print(list_[i])

61dee6c0ff7b364ae89c63f73072a88804e5ec09
tattypaw <67539605+tattypaw@users.noreply.github.com>
2024-10-22 02:16:58 +0800

    Update module_7_5.py

--- module_7/module_7_5.py
+++ module_7/module_7_5.py
@@ -3,9 +3,9 @@ import time
 
 for root, dirs, files in os.walk('.'):
     for file in files:
-        filepath = os.getcwd() + root[1:len(root)]
-        filetime = os.stat(filepath+'\\'+file).st_mtime
+        filepath = root + file
+        filetime = os.stat(os.getcwd() + root[1:len(root)] + '\\' + file).st_mtime
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-        filesize = os.stat(filepath+'\\'+file).st_size
+        filesize = os.stat(os.getcwd() + root[1:len(root)] + '\\' + file).st_size
         parent_dir = root
         print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')

c008bb8e19e676cafcefd6b1cfa846728448e6f7
tattypaw <67539605+tattypaw@users.noreply.github.com>
2024-12-09 20:32:57 +0800

    Update module_11_intro.py

--- module_11/module_11_intro.py
+++ module_11/module_11_intro.py
@@ -1,14 +1,21 @@
 def introspection_info(obj):
+    # Определение типа объекта
     obj_type = type(obj).__name__
-    attributes = dir(obj)
-    methods = [method for method in attributes if callable(getattr(obj, method))]
+    # Получение атрибутов и методов объекта
+    attributes_and_methods = dir(obj)
+    # Получение атрибутов объекта
+    attributes = [attribute for attribute in attributes_and_methods if not callable(getattr(obj, attribute))]
+    # Получение методов объекта
+    methods = [method for method in attributes_and_methods if callable(getattr(obj, method))]
+    # Определение модуля, к которому объект принадлежит
     module = obj.__class__.__module__
+    # Создание словаря с информацией об объекте
     info = {
-        'type': obj_type,
-        'attributes': attributes,
-        'methods': methods,
+        'type': obj_type, 
+        'attributes': attributes, 
+        'methods': methods, 
         'module': module}
     return info
 
-number_info = introspection_info(42)
+number_info = introspection_info(42.45)
 print(number_info)

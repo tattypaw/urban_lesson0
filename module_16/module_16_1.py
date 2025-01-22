6ef975deb68db5ea8f04ecd11928b2aee91f5b8d
tattypaw <67539605+tattypaw@users.noreply.github.com>
2025-01-22 19:09:16 +0800

    Update module_16_1.py

--- module_16/module_16_1.py
+++ module_16/module_16_1.py
@@ -4,16 +4,16 @@ app = FastAPI()
 
 @app.get('/')
 async def root():
-    return {'message': 'Главная страница'}
+    return 'Главная страница'
 
 @app.get('/user/admin')
 async def admin():
-    return {'message': 'Вы вошли как администратор'}
+    return 'Вы вошли как администратор'
 
 @app.get('/user/{user_id}')
 async def users(user_id: int):
-    return {'message': f'Вы вошли как пользователь № {user_id}'}
+    return f'Вы вошли как пользователь № {user_id}'
 
 @app.get('/user')
 async def user_info(username: str = 'User', age: int = 18):
-    return {'message': f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
+    return f'Информация о пользователе. Имя: {username}, Возраст: {age}'
